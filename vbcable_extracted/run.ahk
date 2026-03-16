299fe86728872166d04201df46c718080c0c4889
Remy Skye <remy@example.com>
2026-03-16 12:52:00 +0300

    Split VB-Cable into pack43 and pack45 architectures

---
+++ vbcable_extracted/run.ahk
@@ -0,0 +1,1 @@
+if WinWait("ahk_exe VBCABLE_Setup_x64.exe", , 10) { WinActivate; Sleep 500; Send "{Enter}"; Sleep 3000; Send "{Enter}" }
---
+++ vbcable_extracted/vbcable_choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = "$toolsDir"
+
+  url           = 'https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip'
+  checksum      = '66FD0A4D9F4896FF41632B7E3D53892C085C4561F53E8AE8D0F0BC10EEDD1CDD'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+# Don't create shims for executables
+$files = Get-ChildItem "$toolsDir" -Recurse -Include *.exe
+foreach ($file in $files) {
+  New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+if ((([environment]::OSVersion.version.major) -eq 6) -And (([environment]::OSVersion.version.minor) -eq 1)) {
+  $inf = Join-Path $toolsDir "vbMmeCable_win7.inf"
+} else {
+  $inf = Join-Path $toolsDir "vbMmeCable_2003.inf"
+}
+
+$rundll = Join-Path (Join-Path $Env:SystemRoot "System32") "rundll32.exe"
+Start-ChocolateyProcessAsAdmin "advpack.dll,LaunchINFSectionEx $inf,,,4" "$rundll"
